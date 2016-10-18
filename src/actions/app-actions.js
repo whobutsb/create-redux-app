1fa2b46e35e4f1ca0ff0eea2bd827d6a17861905
Steve Barbera <me@stevebarbera.com>
2016-10-17 19:40:03 -0600

    Added whatwg-fetch and es6-promise
    For AJAX calls

--- src/actions/app-actions.js
+++ src/actions/app-actions.js
@@ -1,10 +1,30 @@
 import * as types from './action-types';
+import 'whatwg-fetch';
 
 export function newAction(){
-    return dispatch => {
-        dispatch({
-            type: types.NEW_ACTION,
-            status: true
-        });
-    };
+  return dispatch => { 
+      return dispatch({
+        type: types.NEW_ACTION,
+        status: true
+      });
+  };
 }
+
+// Sample AJAX POST with Fetch
+// fetch('http://test.com', {
+//   method: 'POST',
+//   credentials: 'same-origin',
+//   headers: new Headers({
+//     'Content-Type': 'application/json',
+//     // 'X-CSRF-Token': APP.csrf_token
+//   }),
+//   mode: 'no-cors',
+//   body: JSON.stringify({ q: 'reactjs' })
+// })
+// .then(function(res){
+//   return res.json();
+// }).then(function(json){
+//   console.log(json);
+// }).catch(function(err){
+//   console.log(err);
+// });
